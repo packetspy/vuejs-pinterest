7a18b9b3bb10b5d917e8518033cc4881bdaceb44
packetspy <packetspy@gmail.com>
2020-01-21 16:37:49 -0300

    create request

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="button-wrapper">
+        <button class="btn" @click="searchUnsplash('Autumn')">Autumn</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from "./components/HelloWorld.vue";
+
+import Axios from "axios";
+import { Stack, StackItem } from "vue-stack-grid";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Stack,
+    StackItem
+  },
+  data: () => ({
+    images: []
+  }),
+  methods: {
+    searchUnsplash(topic) {
+      this.images = [];
+      Axios.get(
+        `https://api.unsplash.com/search/photos?query=${topic}&per_page=20`,
+        {
+          headers: {
+            Authorization: "Client-ID <YourAccessKeyGoesHere>",
+            "Accept-Version": "v1"
+          }
+        }
+      )
+        .then(response => {
+          this.images = response.data.results;
+        })
+        .catch(() => {
+          this.images = [];
+        });
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  width: 80vw;
+  margin: 0 auto;
+}
+.button-wrapper {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 25px;
+}
+.bt {
+  font-size: 18px;
+  background-color: #42b983;
+  color: white;
+  padding: 10px 20px;
 }
 </style>
